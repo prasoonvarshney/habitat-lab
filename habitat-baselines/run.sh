fb0797c52630e175de3d96cef79a77cddd86b2d9
Prasoon Varshney <pvarshne@andrew.cmu.edu>
2023-07-21 01:57:12 -0400

    Verbose error logging and gitignore all video dirs

--- habitat-baselines/run.sh
+++ habitat-baselines/run.sh
@@ -2,6 +2,8 @@
 
 export MAGNUM_LOG=verbose 
 export MAGNUM_GPU_VALIDATION=ON
+export CUDA_LAUNCH_BLOCKING=1
+export HYDRA_FULL_ERROR=1
 
 rm -rf /home/ubuntu/habitat-exp/habitat-lab/video_dir/*.mp4
 
